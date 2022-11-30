0123b21a7265034092be0ce8c4d4493aa8629b3f
crpier42 <crpier42@gmail.com>
2022-11-30 11:53:19 +0200

    allow uploading imges

--- src/components/upload_post.tsx
+++ src/components/upload_post.tsx
@@ -1,11 +1,17 @@
-import { useState, useEffect, ReactElement } from "react";
+import { useState, ReactElement, FormEvent } from "react";
 import ReactDOM from "react-dom";
 import { PostContent } from "./post";
+import { trpc } from "@/utils/trpc";
+import { useSession } from "next-auth/react";
 
 export default function UploadPost(props: { children: ReactElement }) {
+  const postMutation = trpc.post.createPost.useMutation();
   const [modalOpen, setModalOpen] = useState(false);
-  const [fileUploaded, setfileUploaded] = useState(null);
+  const [fileUploaded, setfileUploaded] = useState<any>(null);
   const [createObjectURL, setCreateObjectURL] = useState("");
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState("");
+  const { data: sessionData } = useSession();
 
   const closeModal = () => {
     setModalOpen(false);
@@ -26,8 +32,69 @@ export default function UploadPost(props: { children: ReactElement }) {
       setCreateObjectURL(url);
     }
   };
+
+  const uploadToServer = async (event) => {
+    let fileType: string = fileUploaded.type;
+    if (fileType.includes("/")) {
+      const typeParts = fileType.split("/");
+      fileType = typeParts[typeParts.length - 1] as string;
+    }
+    const userName = sessionData?.user?.name;
+    if (!userName) {
+      return;
+    }
+    postMutation.mutate(
+      {
+        owner: userName,
+        fileType: fileType,
+        tags: tags,
+        postTitle: title,
+      },
+      {
+        onSuccess: async (data, variables, context) => {
+          let resp = await fetch(data, {
+            body: fileUploaded,
+            headers: {
+              "Content-type": fileType,
+              "Access-Control-Allow-Origin": "*",
+            },
+            method: "PUT",
+          });
+          setModalOpen(false);
+          setfileUploaded(null);
+          setCreateObjectURL("");
+          setTitle("");
+          setTags("");
+        },
+      }
+    );
+  };
+
+  const updateTags = (e: FormEvent<HTMLFormElement>) => {
+    const target = e.target as HTMLInputElement;
+    const label = target.labels?.item(0);
+    if (!label) {
+      console.log("Not right");
+      return;
+    }
+    const changedTag = label.innerText;
+    const newTagValue = target.checked;
+    let oldTags = new Set(tags.split(","));
+    if (newTagValue) {
+      oldTags.add(changedTag);
+    } else {
+      oldTags.delete(changedTag);
+    }
+    oldTags.delete("");
+    setTags(Array.from(oldTags).join(","));
+  };
+  const changeTitle = (title: string) => {
+    setTitle(title);
+  };
   if (fileUploaded) {
-    contentSection = <PostContent url={createObjectURL} name={fileUploaded.name}/>;
+    contentSection = (
+      <PostContent url={createObjectURL} name={fileUploaded.name} />
+    );
   } else {
     contentSection = (
       <div className="flex h-64 flex-col items-center rounded border border-dashed bg-gray-900 p-6 text-center">
@@ -49,7 +116,7 @@ export default function UploadPost(props: { children: ReactElement }) {
     modal = ReactDOM.createPortal(
       <div
         className="absolute flex flex-col rounded border bg-gray-700 px-4 pb-4"
-        style={{ left: "30vw", top: "20vh", width: "40vw" }}
+        style={{ left: "30vw", top: "5vh", width: "40vw" }}
       >
         <div className="mt-2 flex justify-end">
           <button
@@ -62,10 +129,14 @@ export default function UploadPost(props: { children: ReactElement }) {
         <p className="mb-1 text-center text-3xl">Upload post</p>
         <div className="mb-4">
           <label className="">Title</label>
-          <input className="w-full rounded border p-1" />
+          <input
+            className="w-full rounded border p-1"
+            value={title}
+            onChange={(e) => changeTitle(e.target.value)}
+          />
         </div>
         {contentSection}
-        <div className="">
+        <form className="" onChange={updateTags}>
           <div>Tags</div>
           <div>
             <input className="mr-2" type="checkbox" id="a" />
@@ -85,6 +156,17 @@ export default function UploadPost(props: { children: ReactElement }) {
               Event
             </label>
           </div>
+        </form>
+        <div className="flex flex-row justify-start">
+          <button className="mt-4 rounded border border-white px-4 py-1 text-sm font-semibold hover:border-transparent hover:bg-white hover:text-teal-500">
+            Cancel
+          </button>
+          <button
+            onClick={uploadToServer}
+            className="mt-4 ml-4 rounded border-white bg-blue-500 px-4 py-1 text-sm font-semibold hover:border-transparent hover:bg-white hover:text-teal-500"
+          >
+            Submit
+          </button>
         </div>
       </div>,
       document.getElementById("modal-root") as HTMLElement
--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -1,6 +1,21 @@
 import { z } from "zod";
 
-import { router, publicProcedure } from "../trpc";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { S3 } from "aws-sdk/clients/all";
+import { Post } from "prisma/prisma-client";
+
+const s3 = new S3({
+  region: "eu-west-1",
+  signatureVersion: "v4",
+});
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "8mb",
+    },
+  },
+};
 
 export const postRouter = router({
   hello: publicProcedure
@@ -13,5 +28,44 @@ export const postRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.post.findMany();
   }),
-});
+  createPost: protectedProcedure
+    .input(
+      z.object({
+        fileType: z.string(),
+        postTitle: z.string(),
+        owner: z.string(),
+        tags: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      let fileName;
 
+      const newPost = await ctx.prisma.$transaction(async (tx) => {
+        let newPost = await tx.post.create({
+          data: {
+            title: input.postTitle,
+            owner: input.owner,
+            tags: input.tags,
+            url: "placeholder",
+          },
+        });
+        fileName = `${newPost.id}.${input.fileType}`;
+        newPost = await tx.post.update({
+          where: {
+            id: newPost.id,
+          },
+          data: {
+            url: `https://misc-personal-projects.s3.eu-west-1.amazonaws.com/memecry${fileName}`,
+          },
+        });
+        return newPost;
+      });
+      return await s3.getSignedUrlPromise("putObject", {
+        Bucket: "misc-personal-projects",
+        Key: `memecry${fileName}`,
+        Expires: 500,
+        ContentType: input.fileType,
+        ACL: "public-read",
+      });
+    }),
+});
